282dfa85f984379b8105a98c2d048f65dc5c8d89
Angus Munro <angusmunro12@gmail.com>
2023-03-17 19:24:02 +1100

    Adding basic add and remove task functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [list, setList] = useState([]);
+	const [input, setInput] = useState("");
+
+	function addTodo(todo) {
+		const newTodo = { id: nanoid(), todo: todo };
+
+		setList([...list, newTodo]);
+		setInput("");
+	}
+
+	function handleInputChange(event) {
+		setInput(event.target.value);
+	}
+
+	function deleteTodo(id) {
+		const newList = list.filter((todo) => todo.id !== id);
+		setList(newList);
+	}
+
+	return (
+		<div>
+			<header>
+				<h1>TodoPad</h1>
+			</header>
+			<input value={input} onChange={handleInputChange} />
+			<button onClick={() => addTodo(input)}>Add</button>
+			<ul>
+				{list.map((todo) => (
+					<li key={todo.id}>
+						{todo.todo}
+						<button onClick={() => deleteTodo(todo.id)}>&times;</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
