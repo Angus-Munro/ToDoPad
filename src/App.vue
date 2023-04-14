0938bac831446024b6ef929b81fc9fedfbb9c402
Angus Munro <angusmunro12@gmail.com>
2023-04-14 20:21:52 +1000

    Initializing Vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainHeader from './components/MainHeader.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <MainHeader msg="TodoPad" />
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
